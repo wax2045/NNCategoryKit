7af435a0b18d14850a6c89ce1d50418ba5c275a9
wax2045 <861412028@qq.com>
2018-04-25 15:30:40 +0800

    abc

---
+++ Example/Pods/Target Support Files/NNCategoryKit/NNCategoryKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_NNCategoryKit : NSObject
+@end
+@implementation PodsDummy_NNCategoryKit
+@end
---
+++ Example/Pods/Target Support Files/NNCategoryKit/NNCategoryKit-umbrella.h
@@ -0,0 +1,18 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "UIButton+Block.h"
+#import "UIView+NNUIKit.h"
+
+FOUNDATION_EXPORT double NNCategoryKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char NNCategoryKitVersionString[];
+
